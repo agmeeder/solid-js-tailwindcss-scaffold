6e5483462fa7c053076b4b056406f44c4ad4fed3
Alexander Meeder <ameeder@gmail.com>
2022-02-17 10:53:45 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Component } from 'solid-js'
+
+const App: Component = () => {
+	return (
+		<>
+			<div class='text-4xl flex items-center justify-center'>Solid-JS with TailwindCSS Scaffold</div>
+		</>
+	)
+}
+
+export default App
